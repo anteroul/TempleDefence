b8c02adc6beb4837bbf4ecf3d98a20b956bce155
Uljas Lindell <antero.lindell@gmail.com>
2023-08-16 21:34:20 +0300

    controller input

--- Assets/Scripts/ManualAim.cs
+++ Assets/Scripts/ManualAim.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ManualAim : MonoBehaviour
 {
@@ -11,28 +11,49 @@ public class ManualAim : MonoBehaviour
     public GameObject projectile;
     public float projectileSpeed = 4.5f;
     public float projectileLifeTime = 4.5f;
-    
-    private ProjectileScript _projectileScript;
 
-    private void Start()
+    ProjectileScript _projectileScript;
+    Vector2 aimDirection;
+    PlayerControls controls;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Gameplay.Fire.performed += ctx => FireTurret();
+        controls.Gameplay.AimTurret.performed += ctx => aimDirection = ctx.ReadValue<Vector2>();
+    }
+
+    void Start()
     {
         _projectileScript = projectile.GetComponent<ProjectileScript>();
     }
 
-    private void Update()
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            _projectileScript.speed = projectileSpeed;
-            _projectileScript.lifeTime = projectileLifeTime;
-            Instantiate(projectile, firePoint.position, transform.rotation);
+            FireTurret();
         }
         RotateTurret();
     }
 
-    private void RotateTurret()
+    void FireTurret()
     {
-        Vector2 distance = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition) - (Vector2)turret.position;
+        _projectileScript.speed = projectileSpeed;
+        _projectileScript.lifeTime = projectileLifeTime;
+        Instantiate(projectile, firePoint.position, transform.rotation);
+    }
+
+    void RotateTurret()
+    {
+        Vector2 direction = Input.mousePosition;
+        //Vector2 direction = new Vector2(aimDirection.x, aimDirection.y);
+        Vector2 distance = (Vector2)cam.ScreenToWorldPoint(direction) - (Vector2)turret.position;
         float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
