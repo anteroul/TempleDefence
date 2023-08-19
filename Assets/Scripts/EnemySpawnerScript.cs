bddcda2a60d6ea85bed891ac2f74e086647071d5
Uljas Lindell <antero.lindell@gmail.com>
2023-08-19 19:03:14 +0300

    enemy spawner

---
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform spawnPoint;
+    public Transform[] path;
+    public float spawnInterval;
+    float spawnTime;
+    float timer;
+
+    void Awake()
+    {
+        enemy.GetComponent<Enemy>().pathPoints = path;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += 0.1f * Time.deltaTime;
+        if (timer > spawnTime)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        spawnTime = Random.Range(1, spawnInterval);
+        timer = 0;
+        Instantiate(enemy, spawnPoint);
+    }
+}
